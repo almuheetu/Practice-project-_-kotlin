ca514c7c8ebd2513cba5afc314d90544ae12f96d
Almuheetu Shihab <almuheetu@gmail.com>
2024-05-15 09:45:13 +0600

    project started

--- src/Main.kt
+++ src/Main.kt
@@ -1,26 +1,3 @@
-//open class Shape(val name: String) {
-//    open fun calculatorArea(): Double {
-//        return 0.0
-//    }
-//}
-//
-//class Rectangle(name: String, private val width: Double, private val height: Double) : Shape(name) {
-//    override fun calculatorArea(): Double {
-//        return width * height
-//    }
-//}
-//
-//class Circle(name: String, val radius: Double) : Shape(name) {
-//    override fun calculatorArea(): Double {
-//        return Math.PI * radius * radius
-//    }
-//}
-//
-//class Triangle(name: String, private val base: Double, private val height: Double) : Shape(name) {
-//    override fun calculatorArea(): Double {
-//        return 0.5 * base * height
-//    }
-//}
 
 
 
@@ -44,52 +21,6 @@
 fun main() {
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    val rectangle = Rectangle("Rectangle", 5.0, 3.0)
-//    val circle = Circle("Circle", 4.0)
-//    val triangle = Triangle("Triangle", 4.0, 6.0)
-//
-//    println("Rectangle area is: ${rectangle.calculatorArea()}")
-//    println("Circle area is: ${circle.calculatorArea()}")
-//    println("Triangle area is: ${triangle.calculatorArea()}")
 
 }
 
