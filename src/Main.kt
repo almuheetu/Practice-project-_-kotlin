cb4eb0f46260b23e8441c2eaa8a51eb836016ee4
Almuheetu Shihab <almuheetu@gmail.com>
2024-05-15 10:52:38 +0600

    all class added

--- src/Main.kt
+++ src/Main.kt
@@ -13,8 +13,17 @@ class Rectangle(name: String, val w: Int, val h: Int) : Shape(name) {
 }
 
 
-class circle(name: String, val )
+class Circle(name: String, val r : Int) : Shape(name){
+    override fun calculatorArea(): Int {
+        return (Math.PI * r * r).toInt()
+    }
+}
 
+class Triangle(name: String, val b : Int, val h: Int) : Shape(name){
+    override fun calculatorArea(): Int {
+        return (0.5 * b * h).toInt()
+    }
+}
 
 fun main() {
 
