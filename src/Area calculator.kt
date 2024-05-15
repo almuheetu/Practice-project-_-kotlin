2068cca941b886ffab310bcbc039b6d74cd0ab3e
Almuheetu Shihab <almuheetu@gmail.com>
2024-05-15 14:23:14 +0600

    extra kotlin file create and stared new pratice project

---
+++ src/Area calculator.kt
@@ -0,0 +1,18 @@
+interface AreaCalculator {
+    fun calculatorArea()
+}
+
+class Reactangle(val name: String, val w:Int, val h: Int, val areaCalculator: AreaCalculator){
+
+}
+
+
+
+
+
+
+fun main() {
+
+
+    println(message = "Hello kotlin")
+}
