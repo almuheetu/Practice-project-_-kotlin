ae3a85ce3c225f57712108aa4be57770999f5c26
Almuheetu Shihab <almuheetu@gmail.com>
2024-05-15 10:06:43 +0600

    shape and rectangle class added

--- src/Main.kt
+++ src/Main.kt
@@ -1,25 +1,22 @@
+open class Shape(val name: String) {
+    open fun calculatorArea(): Int {
+        return 0
+    }
+}
 
 
+class Rectangle(name: String, val w: Int, val h: Int) : Shape(name) {
+
+    override fun calculatorArea(): Int {
+        return w * h
+    }
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class circle(name: String, val )
 
 
 fun main() {
-
 
 
 }
