b2ca97482acffbdf996fb345db7e8f8ba38974a4
Almuheetu Shihab <almuheetu@gmail.com>
2024-05-16 09:29:18 +0600

    some issu problem fixed

--- src/Main.kt
+++ src/Main.kt
@@ -1,35 +1,41 @@
-//open class Shape {
-//    open fun calculatorArea() {
-//        println("default shape")
-//    }
-//}
-//
-//
-//class Rectangle(val name: String, val w: Int, val h: Int) : Shape() {
-//
-//    override fun calculatorArea() {
-//
-//        println("|    $name area is ${w * h }    |")
-//    }
-//}
-//
-//
-//class Circle(val name: String, val r: Int) : Shape() {
-//    override fun calculatorArea()
-//    {
-//
-//
-//        println("|    $name area is ${(Math.PI * r * r).toInt()}    |")
-//    }
-//}
-//
-//class Triangle(val name: String, val b: Int, val h: Int) : Shape() {
-//    override fun calculatorArea() {
-//
-//        println("|    $name area is ${(0.5 * b * h).toInt()}    |")
-//    }
-//}
-import  AreaCalculator
+
+fun interface AreaCalculator {
+    fun calculatorArea()
+}
+
+class Rectangle(val name: String, val width: Int, val height: Int) {
+
+    fun printArea() {
+
+        val area = AreaCalculator {
+            println("|    $name area is ${width*height}    |")
+        }
+
+        area.calculatorArea()
+
+
+    }
+
+}
+
+class Circle(val name: String, val radius: Int) {
+    fun printArea() {
+        val area =  AreaCalculator {
+            println("|    $name area is ${Math.PI*(radius*radius)}    |")
+        }
+        area.calculatorArea()
+
+    }
+}
+
+class Triangle(val name: String, val base: Int, val height: Int) {
+    fun printArea() {
+        val area =    AreaCalculator {
+            println("|    $name area is ${0.5*(base*height)}    |")
+        }
+        area.calculatorArea()
+    }
+}
 
 fun main() {
 
